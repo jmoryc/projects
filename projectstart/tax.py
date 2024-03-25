5a4212f127c1b5220d9e80a85adb8df0713cc8e2
Justyna Moryc <morycjustynaa@gmail.com>
2024-03-25 13:29:12 +0100

    Utworzenie repozytorium

---
+++ projectstart/tax.py
@@ -0,0 +1,12 @@
+income = int(input("Proszę o wprowadzenia dochodu bazowego:"))
+
+if income < 25000:
+    income = income * 0.95
+
+if income > 25000 and income < 50000:
+    income = income * 0.90
+
+if income > 50000:
+    income = income * 0.85
+
+print("Ostateczny dochód po odjęciu podatku wynosi:", income)
---
+++ projectstart/test.py
@@ -0,0 +1,4 @@
+print(bin(42))
+print(0b111111)
+print(0xa4f2)
+print(hex(0x101010))
