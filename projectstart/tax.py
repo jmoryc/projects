4ccd850f0a0dbf0f5d5079a93b2f0bb79122a776
Justyna Moryc <morycjustynaa@gmail.com>
2024-03-25 21:27:18 +0100

    first commit

--- projectstart/tax.py
+++ projectstart/tax.py
@@ -6,7 +6,8 @@ if income < 25000:
 if income > 25000 and income < 50000:
     income = income * 0.90
 
-if income > 50000:
+if income > 50000 and < 60000:
     income = income * 0.85
+
 
 print("Ostateczny dochód po odjęciu podatku wynosi:", income)
